39a35d645d3a7c82cbe3c8b41899f5a5770d312b
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-06 11:55:06 -0600

    AVAudioPlayerDelegate added

--- SimpleAudioRecorder/ViewController.swift
+++ SimpleAudioRecorder/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func play(_ sender: Any) {
+        
+        defer { updateButton() }
+        
+        guard let audioURL = Bundle.main.url(forResource: "empire-of-the-sun-walking-on-a-dream", withExtension: "mp3") else { return }
+        
+        guard !isPlaying else {
+            player?.pause()
+            return
+        }
+        
+        if player != nil && !isPlaying {
+            player?.play()
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: audioURL)
+            player?.play()
+        } catch {
+            NSLog("Unable to play audio: \(error.localizedDescription)")
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.player = nil
+        updateButton()
+    }
+    
+    func updateButton() {
+        let playButtonTitle = isPlaying ? "Stop" : "Play"
+        playButton.setTitle(playButtonTitle, for: .normal)
+    }
+    
+    private var player: AVAudioPlayer?
+    private var isPlaying: Bool {
+        return player?.isPlaying ?? false
+    }
+    @IBOutlet weak var playButton: UIButton!
 }
 
