91ea690d0a95224d0b1d173ae54f681988452bdd
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-06 11:16:29 -0600

    Initial Commit

---
+++ SimpleAudioRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleAudioRecorder
+//
+//  Created by Ilgar Ilyasov on 11/6/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
